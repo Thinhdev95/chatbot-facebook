d18ade19333b8f8a2e8d29f184388d5678198a19
Lê Thịnh <thinhdev95@gmail.com>
2021-04-16 21:26:03 +0700

    push token

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/webhook', function(req, res) {
-  if (req.query['hub.verify_token'] === 'anh_hoang_dep_trai_vo_doi') {
+  if (req.query['hub.verify_token'] === 'thinh_dev_95') {
     res.send(req.query['hub.challenge']);
   }
   res.send('Error, wrong validation token');
