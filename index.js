6fe96bea367d8018b22db45e6e0a18cda604a983
Lê Thịnh <thinhdev95@gmail.com>
2021-04-19 23:07:05 +0700

    update

--- index.js
+++ index.js
@@ -46,13 +46,6 @@ function handleMessage(sender_psid, received_message) {
     let textreponse = "Hello bạn , tôi là bot đây";
     if (received_message.text) {  
         console.log(received_message.text)
-        // switch(received_message.text){
-        //     case "Hello": textreponse = "Hello bạn"
-        //     break;
-        //     case "Hi": textreponse = "Hi bạn"
-        //     break;
-        //     default: textreponse = "Bạn nói gì vậy"
-        // }
         switch(true){
             case received_message.text.includes("Hello"): textreponse = "Hello bạn"
             break;
@@ -89,9 +82,6 @@ function callSendAPI(sender_psid, response) {
         }
     });
 }
-// app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
-// app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "127.0.0.1");
-// server.listen(app.get('port'), app.get('ip'), function() {
 server.listen(process.env.PORT, function () {
     console.log("Chat bot server listening at %s:%d ", app.get('ip'), app.get('port'));
 });
